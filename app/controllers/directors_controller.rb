cefa58ed2ee3f9e6bebdc46c4b8c6cf9d52f3d7b
vmancode <146589771+vmancode@users.noreply.github.com>
2023-12-07 03:49:00 +0000

    commit 4

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,5 +1,18 @@
 class DirectorsController < ApplicationController
 
+
+
+  def junior
+    @young_director = Director.where.not({ :dob => nil }).order(dob: :desc).first
+    render({ :template => "director_templates/young"})
+  end
+
+  def elder
+    @eldest_director = Director.where.not({ :dob => nil }).order(dob: :asc).first
+    render({ :template => "director_templates/old"})
+  end
+
+
   def index
     render({ :template => "director_templates/list"})
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,21 @@ Rails.application.routes.draw do
 
 get("/directors", { :controller => "directors", :action => "index"})
 
+
+get("/directors/youngest", { :controller => "directors", :action => "junior"})
+get("/directors/eldest", { :controller => "directors", :action => "elder"})
+
+
 get("/directors/:the_id", { :controller => "directors", :action => "show"})
+
+get("/actors", { :controller => "actors", :action => "index"})
+
+get("/actors/:the_id", { :controller => "actors", :action => "show"})
+
+
+
+
+
 
 get("/movies", { :controller => "movies", :action => "index"})
 
